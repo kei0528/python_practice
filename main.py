73f794517ec17e70aa38ef29e36844009623eb6a
Keisuke Tanaka <keisuketanaka97@gmail.com>
2022-11-20 20:31:11 +0100

    add bmi calculator

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
 # 🚨 Don't change the code below 👇
-number = int(input("Which number do you want to check? "))
-# 🚨 Don't change the code above 👆
-
-#Write your code below this line 👇
-
-
-if number % 2 == 0:
-  print('This is an even number')
-else:
-  print('This is an odd number.')
+name = input('What is your name?')
+age = int(input("How old are you? "))
