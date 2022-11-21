f72a2b2a2298f215f3d394e05ebdc39ef0f47716
Keisuke Tanaka <keisuke@octily.com>
2022-11-21 19:31:09 +0100

    add tresure map

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-# 🚨 Don't change the code below 👇
-name = input('What is your name?')
-age = int(input("How old are you? "))
+import random
+# Import the random module here
+
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+target_name = names[random.randint(0, len(names) - 1)]
+print(f'{target_name} is going to buy the meal today!')
